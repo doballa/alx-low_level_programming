16444442a8699cbc6eb2930ab23133d6f7090153
doballa <dennisobala@gmail.com>
2022-09-16 04:46:16 -0700

    Using putchar

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int i = 0;
+	while (s[i] != '\0')
+	{
+		putchar(s[i]);
+		i++;
+	}
+
+	return (0);
+}
